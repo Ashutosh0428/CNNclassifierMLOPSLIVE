6f8a7b1b478bf23178b9695d8662aecd402d00f5
Ashutosh Sharma <kapil.shrm1.kks@gmail.com>
2022-09-19 22:58:34 +0530

    update

--- src/deepClassifier/config/configuration.py
+++ src/deepClassifier/config/configuration.py
@@ -1,13 +1,6 @@
 from deepClassifier.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH
 from deepClassifier.utils import read_yaml, create_directories
-from deepClassifier.entity import (
-    DataIngestionConfig, 
-    PrepareBaseModelConfig, 
-    PrepareCallbacksConfig,
-    TrainingConfig
-)
-from pathlib import Path
-import os
+from deepClassifier.entity import DataIngestionConfig
 
 class ConfigurationManager:
     def __init__(
